76f8ac48a171f712598a7cb4635bf122acdb727f
Isaac Obuya <Szack4477@gmail.com>
2019-09-05 14:14:50 +0300

    implement filter functionality

--- todo-app.js
+++ todo-app.js
@@ -23,14 +23,50 @@ const todos = [{
     text : 'Excercise',
     completed : true
 }];
-//Return incoplete tasks
-const incomplete = todos.filter(function(todo) {
-    return todo.completed === false;
-})
-console.log(incomplete);
 
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incomplete.length} todos left`;
-document.querySelector('body').appendChild(summary);
+const filters = {
+    searchText: ''
+};
+
+const renderTodos = function(notes,filters) {
+    const filterdTodos = todos.filter(function(todo) {
+        return todo.text.toUpperCase().includes(filters.searchText.toUpperCase());
+    });
+        //Return incoplete tasks
+        const incomplete = filterdTodos.filter(function(todo) {
+            return todo.completed === false;
+        })
+         document.querySelector('#todos').innerHTML = '';
+
+        const summary = document.createElement('h2');
+        summary.textContent = `You have ${incomplete.length} todos left`;
+        document.querySelector('#todos').appendChild(summary);
+
+        filterdTodos.forEach(function(todo) {
+            const p = document.createElement('p');
+            p.textContent = todo.text;
+            document.querySelector('#todos').appendChild(p);
+        });    
+
+}
+
+renderTodos(todos,filters);
+
+
+//Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log("Add a new todo");
+})
+
+//Listen for todo text
+document.querySelector('#new-todo-text').addEventListener('input', function(e) {
+    console.log(e.target.value);
+})
+
+//search for todos
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value;
+    renderTodos(todos,filters);
+})
 
 
